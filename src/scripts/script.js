3f171d1a8b1a9ae9341aaf49b29dd73e49cb03ac
RizqiMKS <rizqimks@gmail.com>
2022-12-14 19:20:19 +0700

    Moving swal code to script.js, adding setInterval for more fluid response

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -27,5 +27,26 @@ let weather = {
 };
 
 document.querySelector("#search").addEventListener("click", () => {
-  weather.search()
+  Swal.fire({
+    title: 'Mencari data',
+    html: 'Mohon tunggu <b></b> milliseconds.',
+    timer: 1000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading()
+      const b = Swal.getHtmlContainer().querySelector('b')
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+      }, 100)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log('I was closed by the timer')
+    }
+  })
+  setTimeout(weather.search, 1000)
 })
